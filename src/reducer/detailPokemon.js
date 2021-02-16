d979563e87678f77d0df1ee29a3a7f75f0ad97b4
ferryal <ferryal.bahari@amartha.com>
2021-02-16 11:14:05 +0700

    add reducer detail pokemon

---
+++ src/reducer/detailPokemon.js
@@ -0,0 +1,31 @@
+import { POKEMONDETAIL } from '../actions/ActionTypes';
+
+const initialState = {
+  loading: false,
+  detail: {
+    name: ''
+  }
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case POKEMONDETAIL.LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case POKEMONDETAIL.FETCH_DETAIL_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        detail: action.payload.data
+      };
+    case POKEMONDETAIL.FETCH_DETAIL_FAILED:
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
+  }
+};
