74c8932072fbc592622793d6a39657839bef33d1
ferryal <ferryal.bahari@amartha.com>
2021-02-18 09:33:41 +0700

    fix: fix linter reducer

--- src/reducer/detailPokemon.js
+++ src/reducer/detailPokemon.js
@@ -3,29 +3,34 @@ import { POKEMONDETAIL } from '../actions/ActionTypes';
 const initialState = {
   loading: false,
   detail: {
-    name: ''
-  }
+    name: '',
+  },
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case POKEMONDETAIL.LOADING:
-      return {
-        ...state,
-        loading: true
-      };
-    case POKEMONDETAIL.FETCH_DETAIL_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        detail: action.payload.data
-      };
-    case POKEMONDETAIL.FETCH_DETAIL_FAILED:
-      return {
-        ...state,
-        loading: false
-      };
-    default:
-      return state;
+  case POKEMONDETAIL.LOADING:
+    return {
+      ...state,
+      loading: true,
+    };
+  case POKEMONDETAIL.FETCH_DETAIL_SUCCESS:
+    return {
+      ...state,
+      loading: false,
+      detail: action.payload.data,
+    };
+  case POKEMONDETAIL.FETCH_DETAIL_FAILED:
+    return {
+      ...state,
+      loading: false,
+    };
+  case POKEMONDETAIL.RESET_DETAIL:
+    return {
+      ...state,
+      detail: initialState.detail,
+    };
+  default:
+    return state;
   }
 };
--- src/reducer/listPokemon.js
+++ src/reducer/listPokemon.js
@@ -3,33 +3,33 @@ import { LISTPOKEMON } from '../actions/ActionTypes';
 const initialState = {
   loading: false,
   listPokemon: {},
-  pokemon: []
+  pokemon: [],
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LISTPOKEMON.LOADING:
-      return {
-        ...state,
-        loading: true
-      };
-    case LISTPOKEMON.FETCH_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-				listPokemon: action.payload.data.results
-      };
-    case LISTPOKEMON.FETCH_FAILED:
-      return {
-        ...state,
-        loading: false
-      };
-    case LISTPOKEMON.FETCH_IMAGE_SUCCESS:
-      return {
-        ...state,
-        pokemon: [...state.pokemon, action.payload.data]
-      };
-    default:
-      return state;
+  case LISTPOKEMON.LOADING:
+    return {
+      ...state,
+      loading: true,
+    };
+  case LISTPOKEMON.FETCH_SUCCESS:
+    return {
+      ...state,
+      loading: false,
+      listPokemon: action.payload.data.results,
+    };
+  case LISTPOKEMON.FETCH_FAILED:
+    return {
+      ...state,
+      loading: false,
+    };
+  case LISTPOKEMON.FETCH_IMAGE_SUCCESS:
+    return {
+      ...state,
+      pokemon: [...state.pokemon, action.payload.data],
+    };
+  default:
+    return state;
   }
 };
